603f9808ebd40212ea2daf2f264b13697cecb881
Donquixotegarry <zhengji20001205@gmail.com>
2020-10-17 17:04:38 +0800

    tiny commit

--- lab1/linux-eg/egclient.cpp
+++ lab1/linux-eg/egclient.cpp
@@ -12,10 +12,14 @@ int main(){
 
     //向服务器（特定的IP和端口）发起请求
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
-    serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.137.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+    memset(&serv_addr, 0, sizeof(serv_addr));  
+        //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;  
+        //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.137.1");  
+        //具体的IP地址
+    serv_addr.sin_port = htons(1234);  
+        //端口
     
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
    
--- lab1/linux-eg/egserver.cpp
+++ lab1/linux-eg/egserver.cpp
@@ -13,10 +13,14 @@ int main(){
 
     //将套接字和IP、端口绑定
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
-    serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.137.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+    memset(&serv_addr, 0, sizeof(serv_addr));
+        //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;
+        //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.137.1");  
+        //具体的IP地址
+    serv_addr.sin_port = htons(1234);  
+        //端口
     
     //绑定服务器地址
     bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
