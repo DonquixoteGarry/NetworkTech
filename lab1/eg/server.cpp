4e19286624c2c614b20a3597af1d3e3f784fc65d
Donquixotegarry <zhengji20001205@gmail.com>
2020-10-24 00:49:45 +0800

    no info

--- lab1/eg/server.cpp
+++ lab1/eg/server.cpp
@@ -5,28 +5,42 @@
   
 int main(int argc, char* argv[])  
 {  
-    //init WSA  
+    //initialize WSA
+    //use Winsock 2.2 Version  
     WORD sockVersion = MAKEWORD(2,2);
+
+    //use WSADATA to store the data of socket
     WSADATA wsaData;  
+    
+    //declare to use certain lib and bind it with this program
+    //if fail, means can't bind them or can't find the lib 
     if(WSAStartup(sockVersion, &wsaData)!=0)  
     {  
         return 0;  
     }  
   
     //create socket  
+    //address format - AF_INET
+    //socket type - SOCK_STREAM 
+    //protocol - SIPPROTO_TCP
     SOCKET slisten = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);  
+    
+    //if invaild, socket-creation fail
     if(slisten == INVALID_SOCKET)  
     {  
         printf("socket error !");  
         return 0;  
     }  
   
-    //bind IP and port  
+    //bind IP and port
+    //use sockaddr_in as a argument of socket
+    //sockaddr_in contain the data about address
     sockaddr_in sin;  
     sin.sin_family = AF_INET;  
-    sin.sin_port = htons(8888);  
-    // sin.sin_addr.S_un.S_addr = INADDR_ANY; 
-	sin.sin_addr.s_addr = INADDR_ANY;  
+    sin.sin_port = htons(8888);
+      
+    //INADDR_ANY contain all ip address (as same as 0.0.0.0)
+	sin.sin_addr.s_addr = INADDR_ANY; 
     if(bind(slisten, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)  
     {  
         printf("bind error !");  
