2fa6fca4aee059f91fa4a3db9e09fb39b36d8aed
Donquixotegarry <zhengji20001205@gmail.com>
2020-12-08 15:13:50 +0800

    lab3-1 tiny commit in 12-08

--- lab3-1/client/client.cpp
+++ lab3-1/client/client.cpp
@@ -11,21 +11,44 @@ int main()
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2, 2), &wsadata);
 	
-	SOCKET client_sock = socket(AF_INET, SOCK_DGRAM, 0);
-    SOCKADDR_IN server_addr;
+	SOCKET client_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
+	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(1234);
 
 	char filename_buffer[FILENAME_MAXLEN];
-	
-	//int socket_len=sizeof(SOCKADDR);
 
     printf("please give the file's name to download:");
     scanf("%s", filename_buffer);
     int filename_len = strlen(filename_buffer);
-    filename_buffer[filename_len] = '\0';
+    
+    sendto(client_sock, filename_buffer,
+            FILENAME_MAXLEN, 0, 
+            (SOCKADDR *)&server_addr, 
+            sizeof(server_addr));
+    
+    printf("now sending the transferring request to the server\n",filename_buffer);
+    
+    char exist_info[MAXLEN];
+    
+    SOCKADDR recvfrom_addr;
+    int socket_len = sizeof(recvfrom_addr);
+    
+    int recv_info_len=
+        recvfrom(client_sock,exist_info,
+                    MAXLEN,0, 
+                    (SOCKADDR *)&recvfrom_addr, 
+                    &socket_len);
+    
+    if(exist_info[0]=='F')
+    {
+    	printf("file '%s' doesn't exist\n",filename_buffer);
+		closesocket(client_sock);
+		system("pause");
+        return 0;
+	}
     
 	printf("now creating a empty file to receive target file..\n");
 	
@@ -39,27 +62,24 @@ int main()
     }
 
 	printf("empty file creating OK\n");
-        
-    sendto(client_sock, filename_buffer,FILENAME_MAXLEN, 0, 
-        (SOCKADDR *)&server_addr, sizeof(server_addr));
-    
-    printf("now sending the transferring request to the server\n",filename_buffer);
-    
-    SOCKADDR recvfrom_addr;
-    int socket_len = sizeof(recvfrom_addr);
-    
     printf("ready for receiving '%s'\n",filename_buffer);
     
     int file_content_len;
     char file_download_buffer[MAXLEN];
 	bool is_downloaded=false;
+	
 	while ((is_downloaded==false)&&
-                (file_content_len = recvfrom(client_sock,file_download_buffer,MAXLEN,0, 
-                (SOCKADDR *)&recvfrom_addr, &socket_len)) > 0 )
+            (file_content_len = 
+                recvfrom(client_sock,
+                    file_download_buffer,
+                    MAXLEN,0, 
+                    (SOCKADDR *)&recvfrom_addr, 
+                    &socket_len)) 
+            > 0 )
     {
-    	//printf("received file content length = %d\n", file_content_len);
         printf("downloaded content:\n%s\n",file_download_buffer);
-        fwrite(file_download_buffer, 1, file_content_len, fp);
+        fwrite(file_download_buffer, 1, 
+                file_content_len, fp);
         memset(file_download_buffer,0,MAXLEN);
         is_downloaded=true;
     }
--- lab3-1/server/server.cpp
+++ lab3-1/server/server.cpp
@@ -11,7 +11,7 @@ int main()
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2, 2), &wsadata);
 
-    SOCKET server_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    SOCKET server_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
@@ -22,45 +22,60 @@ int main()
     
     bind(server_sock, (SOCKADDR *)&server_addr, sizeof(server_addr));
     
-    
     SOCKADDR client_addr;
     int socket_addr_len = sizeof(client_addr);
-    int filename_receive_len = recvfrom(server_sock, filename_buffer, 
-					FILENAME_MAXLEN, 0, &client_addr, &socket_addr_len);
+    int filename_receive_len = 
+        recvfrom(server_sock, filename_buffer, 
+					FILENAME_MAXLEN, 0, 
+                    &client_addr, 
+                    &socket_addr_len);
     
     printf("now received a transferring request from client called %s\n",filename_buffer);
     
     FILE *fp = fopen(filename_buffer, "rb");
     
+    char file_exist[MAXLEN]="T\0";
+    char file_not_exist[MAXLEN]="F\0";
+    
+    printf("client request to download ':%s''\n", filename_buffer);
     
 	if (fp == NULL)
     {
         printf("local file '%s' open error,maybe it doesn't exist\n",filename_buffer);
-        system("pause");
+        sendto(server_sock, file_not_exist, 
+                MAXLEN, 0, &client_addr, 
+                socket_addr_len);
 		fclose(fp);
 		closesocket(server_sock);
+		system("pause");
         return 0;
     }
     
+    sendto(server_sock, file_exist, 
+            MAXLEN, 0, &client_addr, 
+            socket_addr_len);
     
-	printf("client request to download ':%s''\n", filename_buffer);
     printf("ready for transferring '%s'\n",filename_buffer);
     
 	int file_content_len;
     char file_download_buffer[MAXLEN];
     bool is_transferred=false;
+    
 	while ((is_transferred==false)
-			&&(file_content_len = fread(file_download_buffer,1,MAXLEN,fp) > 0))
+			&&(file_content_len = 
+                fread(file_download_buffer,
+                    1,MAXLEN,fp) 
+                > 0))
     {
-        //printf("file content length = %d\n", file_content_len);
-        printf("transferring content:\n%s\n",file_download_buffer);
-        sendto(server_sock, file_download_buffer, MAXLEN, 0, 
-                    &client_addr, socket_addr_len);
+		printf("transferring content:\n%s\n",file_download_buffer);
+        sendto(server_sock, file_download_buffer,
+                 MAXLEN, 0, &client_addr, 
+                 socket_addr_len);
         memset(file_download_buffer,0,MAXLEN);
-        is_transferred=true;
+		is_transferred=true;
     }
     
-    if(is_transferred==true)
+    if((1)&&(is_transferred==true))
 		printf("transferring OK\n");
 		
 	fclose(fp);
